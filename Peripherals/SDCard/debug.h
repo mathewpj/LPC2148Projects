36c848f5385f7af64d493ef52e485ff0b556878a
X <you@example.com>
2018-02-26 00:51:49 +0530

    Adding Support for FatFS module

---
+++ Peripherals/SDCard/debug.h
@@ -0,0 +1,17 @@
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+/* Initilize an array with ASCII values of 0 to F */
+// 0 = 48
+// ...
+// 9 = 57
+// A = 65
+// ...
+// 70 = F  
+char ascii_string[16] ={48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70};
+
+unsigned char hn, ln, temp;
+uint8_t	send[2];
+
+
+#endif //_DEBUG_H_
